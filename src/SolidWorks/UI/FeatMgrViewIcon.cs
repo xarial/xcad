2b2d47e4d7ff2d4a5c16a72a43d4e404c4f2f4c0
Artem <artem.taturevych@xarial.com>
2020-07-19 11:17:10 +1000

    Added license headers
    Changed the preview feed to be on public feed

--- src/SolidWorks/UI/FeatMgrViewIcon.cs
+++ src/SolidWorks/UI/FeatMgrViewIcon.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections.Generic;
+﻿//*********************************************************************
+//xCAD
+//Copyright(C) 2020 Xarial Pty Limited
+//Product URL: https://www.xcad.net
+//License: https://xcad.xarial.com/license/
+//*********************************************************************
+
+using System.Collections.Generic;
 using System.Drawing;
 using Xarial.XCad.SolidWorks.Base;
 
