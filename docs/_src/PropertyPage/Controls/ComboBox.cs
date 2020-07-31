bc5b36edf59823f1a7192d1113d722fd2edac3f8
Artem <artem.taturevych@xarial.com>
2020-07-31 18:54:55 +1000

    Added example for combobox dynamic items

--- docs/_src/PropertyPage/Controls/ComboBox.cs
+++ docs/_src/PropertyPage/Controls/ComboBox.cs
@@ -1,6 +1,9 @@
 ﻿using SolidWorks.Interop.swconst;
+using System.Collections.Generic;
 using Xarial.XCad.Base.Attributes;
 using Xarial.XCad.Documentation.Properties;
+using Xarial.XCad.SolidWorks;
+using Xarial.XCad.SolidWorks.UI.PropertyPage.Services;
 using Xarial.XCad.UI.PropertyPage.Attributes;
 using Xarial.XCad.UI.PropertyPage.Enums;
 
@@ -28,5 +31,24 @@ public class ComboBoxDataModel
     }
 
     public OptionsCustomized_e Options2 { get; set; }
+    //---
+    //--- CustomItemsProvider
+    public class CustomStringItemsProvider : SwCustomItemsProvider<string>
+    {
+        public override IEnumerable<string> ProvideItems(SwApplication app)
+            => new string[] { "A", "B", "C" };
+    }
+
+    public class CustomIntItemsProvider : SwCustomItemsProvider<int>
+    {
+        public override IEnumerable<int> ProvideItems(SwApplication app)
+            => new int[] { 1, 2, 3 };
+    }
+
+    [CustomItems(typeof(CustomStringItemsProvider))]
+    public string Options3 { get; set; } = "B";
+
+    [CustomItems(typeof(CustomIntItemsProvider))]
+    public int Options4 { get; set; }
     //---
 }
