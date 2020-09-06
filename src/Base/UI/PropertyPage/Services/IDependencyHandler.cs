0a7cded284d6949f64f877a613473d2738779b18
Artem <artem.taturevych@xarial.com>
2020-09-06 20:24:52 +1000

    Added license headers

--- src/Base/UI/PropertyPage/Services/IDependencyHandler.cs
+++ src/Base/UI/PropertyPage/Services/IDependencyHandler.cs
@@ -1,4 +1,11 @@
-﻿using Xarial.XCad.UI.PropertyPage.Base;
+﻿//*********************************************************************
+//xCAD
+//Copyright(C) 2020 Xarial Pty Limited
+//Product URL: https://www.xcad.net
+//License: https://xcad.xarial.com/license/
+//*********************************************************************
+
+using Xarial.XCad.UI.PropertyPage.Base;
 
 namespace Xarial.XCad.UI.PropertyPage.Services
 {
