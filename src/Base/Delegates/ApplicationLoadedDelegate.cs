ab390df03656a25fd75977072b53c46f25e9423b
Artem <artem.taturevych@xarial.com>
2020-10-14 18:14:49 +1100

    Implemented #33 - Add event when extension and host application is fully loaded
    Implemented #34 - Add WindowRectangle API to find the bounds of the host window

---
+++ src/Base/Delegates/ApplicationLoadedDelegate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xarial.XCad.Delegates
+{
+    /// <summary>
+    /// Delegate handler of <see cref="IXApplication.Loaded"/> event
+    /// </summary>
+    /// <param name="app">Extension</param>
+    public delegate void ApplicationLoadedDelegate(IXApplication app);
+}
