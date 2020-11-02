f8e1947b3e65d927c4aba511bcaf0d215475157a
Artem <artem.taturevych@xarial.com>
2020-11-02 14:10:35 +1100

    Created LazyNewDocumentGeometryBuilderDocumentProvider

--- src/SolidWorks/Services/IMemoryGeometryBuilderDocumentProvider.cs
+++ src/SolidWorks/Services/IMemoryGeometryBuilderDocumentProvider.cs
@@ -10,31 +10,4 @@ namespace Xarial.XCad.SolidWorks.Services
     {
         SwDocument ProvideDocument(Type geomType);
     }
-
-    internal class DefaultMemoryGeometryBuilderDocumentProvider : IMemoryGeometryBuilderDocumentProvider
-    {
-        private readonly SwApplication m_App;
-
-        internal DefaultMemoryGeometryBuilderDocumentProvider(SwApplication app)
-        {
-            m_App = app;
-        }
-
-        public SwDocument ProvideDocument(Type geomType)
-        {
-            var part = m_App.Documents.Active as SwPart;
-
-            if (part == null)
-            {
-                part = m_App.Documents.OfType<SwPart>().FirstOrDefault();
-            }
-
-            if (part == null)
-            {
-                throw new Exception("Failed to find part document for memory geometry builder");
-            }
-
-            return part;
-        }
-    }
 }
---
+++ src/SolidWorks/Services/LazyNewDocumentGeometryBuilderDocumentProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xarial.XCad.Documents;
+using Xarial.XCad.SolidWorks.Documents;
+
+namespace Xarial.XCad.SolidWorks.Services
+{
+    public class LazyNewDocumentGeometryBuilderDocumentProvider : IMemoryGeometryBuilderDocumentProvider
+    {
+        protected readonly SwApplication m_App;
+
+        private SwDocument m_TempDoc;
+
+        public LazyNewDocumentGeometryBuilderDocumentProvider(SwApplication app)
+        {
+            m_App = app;
+        }
+
+        public SwDocument ProvideDocument(Type geomType) => GetTempDocument();
+
+        protected virtual SwDocument CreateTempDocument()
+        {
+            var activeDoc = m_App.Documents.Active;
+
+            var doc = (SwDocument)m_App.Documents.NewPart();
+            doc.Title = "xCADGeometryBuilderDoc_" + Guid.NewGuid().ToString();
+            
+            if (activeDoc != null) 
+            {
+                m_App.Documents.Active = activeDoc;
+            }
+
+            doc.Visible = false;
+
+            return doc;
+        }
+
+        private SwDocument GetTempDocument()
+        {
+            if (!IsTempDocAlive())
+            {
+                m_TempDoc = CreateTempDocument();
+            }
+
+            return m_TempDoc;
+        }
+
+        private bool IsTempDocAlive()
+        {
+            if (m_TempDoc != null)
+            {
+                try
+                {
+                    var title = m_TempDoc.Title;
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
