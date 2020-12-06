9246ce9dfde5e5fc25b534744cedf20b47192808
Artem <artem.taturevych@xarial.com>
2020-12-06 17:00:26 +1100

    Fixed the save error message extraction

--- src/SolidWorks/Documents/Exceptions/SaveDocumentFailedException.cs
+++ src/SolidWorks/Documents/Exceptions/SaveDocumentFailedException.cs
@@ -9,35 +9,74 @@ namespace Xarial.XCad.SolidWorks.Documents.Exceptions
     {
         private static string ParseSaveError(swFileSaveError_e err)
         {
-            switch (err)
+            var errors = new StringBuilder();
+
+            if (err.HasFlag(swFileSaveError_e.swFileLockError))
             {
-                case swFileSaveError_e.swFileLockError:
-                    return "File lock error";
-                case swFileSaveError_e.swFileNameContainsAtSign:
-                    return "File name cannot contain the at symbol(@)";
-                case swFileSaveError_e.swFileNameEmpty:
-                    return "File name cannot be empty";
-                case swFileSaveError_e.swFileSaveAsBadEDrawingsVersion:
-                    return "Bad eDrawings data";
-                case swFileSaveError_e.swFileSaveAsDoNotOverwrite:
-                    return "Cannot overwrite an existing file";
-                case swFileSaveError_e.swFileSaveAsInvalidFileExtension:
-                    return "File name extension does not match the SOLIDWORKS document type";
-                case swFileSaveError_e.swFileSaveAsNameExceedsMaxPathLength:
-                    return "File name cannot exceed 255 characters";
-                case swFileSaveError_e.swFileSaveAsNotSupported:
-                    return "Save As operation is not supported in this environment";
-                case swFileSaveError_e.swFileSaveFormatNotAvailable:
-                    return "Save As file type is not valid";
-                case swFileSaveError_e.swFileSaveRequiresSavingReferences:
-                    return "Saving an assembly with renamed components requires saving the references";
-                case swFileSaveError_e.swGenericSaveError:
-                    return "Generic error";
-                case swFileSaveError_e.swReadOnlySaveError:
-                    return "File is readonly";
-                default:
-                    return "Unknown error";
+                errors.AppendLine("File lock error");
             }
+
+            if (err.HasFlag(swFileSaveError_e.swFileNameContainsAtSign))
+            {
+                errors.AppendLine("File name cannot contain the at symbol(@)");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileNameEmpty))
+            {
+                errors.AppendLine("File name cannot be empty");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveAsBadEDrawingsVersion))
+            {
+                errors.AppendLine("Bad eDrawings data");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveAsDoNotOverwrite))
+            {
+                errors.AppendLine("Cannot overwrite an existing file");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveAsInvalidFileExtension))
+            {
+                errors.AppendLine("File name extension does not match the SOLIDWORKS document type");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveAsNameExceedsMaxPathLength))
+            {
+                errors.AppendLine("File name cannot exceed 255 characters");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveAsNotSupported))
+            {
+                errors.AppendLine("Save As operation is not supported in this environment");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveFormatNotAvailable))
+            {
+                errors.AppendLine("Save As file type is not valid");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swFileSaveRequiresSavingReferences))
+            {
+                errors.AppendLine("Saving an assembly with renamed components requires saving the references");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swGenericSaveError))
+            {
+                errors.AppendLine("Generic error");
+            }
+
+            if (err.HasFlag(swFileSaveError_e.swReadOnlySaveError))
+            {
+                errors.AppendLine("File is readonly");
+            }
+
+            if (errors.Length == 0) 
+            {
+                errors.Append("Unknown error");
+            }
+
+            return errors.ToString();
         }
 
         public swFileSaveError_e ErrorCode { get; }
