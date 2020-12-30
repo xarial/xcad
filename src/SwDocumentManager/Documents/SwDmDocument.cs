6f35c147129d19a523cbf1ca7fabb3043797f6f9
Artem <artem.taturevych@xarial.com>
2020-12-30 23:03:16 +1100

    Added license headers

--- src/SwDocumentManager/Documents/SwDmDocument.cs
+++ src/SwDocumentManager/Documents/SwDmDocument.cs
@@ -1,4 +1,11 @@
-﻿using SolidWorks.Interop.swdocumentmgr;
+﻿//*********************************************************************
+//xCAD
+//Copyright(C) 2020 Xarial Pty Limited
+//Product URL: https://www.xcad.net
+//License: https://xcad.xarial.com/license/
+//*********************************************************************
+
+using SolidWorks.Interop.swdocumentmgr;
 using System;
 using System.Collections.Generic;
 using System.IO;
