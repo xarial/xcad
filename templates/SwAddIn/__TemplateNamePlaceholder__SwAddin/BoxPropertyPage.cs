e885049d547e47e06a3368b5f2a0dbab40dc325a
Artem <artem.taturevych@xarial.com>
2022-12-27 17:29:46 +1100

    Added property page

---
+++ templates/SwAddIn/__TemplateNamePlaceholder__SwAddin/BoxPropertyPage.cs
@@ -0,0 +1,47 @@
+﻿using __TemplateNamePlaceholder__SwAddin.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XCad.Base.Attributes;
+using Xarial.XCad.SolidWorks.UI.PropertyPage;
+using Xarial.XCad.UI.PropertyPage.Attributes;
+using Xarial.XCad.UI.PropertyPage.Enums;
+
+namespace __TemplateNamePlaceholder__SwAddin
+{
+    [ComVisible(true)]
+    [Guid("1825D395-A8A2-484F-B9D6-C2D04B2AB9B3")]
+    [Icon(typeof(Resources), nameof(Resources.box_icon))]
+    [Title("Create Box")]
+    public class BoxPropertyPage : SwPropertyManagerPageHandler
+    {
+        public class ParametersGroup
+        {
+            [NumberBoxOptions(NumberBoxUnitType_e.Length, 0, 1000, 0.01, false, 0.02, 0.001)]
+            [Description("Width of the box")]
+            [StandardControlIcon(BitmapLabelType_e.Width)]
+            public double Width { get; set; } = 0.1;
+
+            [NumberBoxOptions(NumberBoxUnitType_e.Length, 0, 1000, 0.01, false, 0.02, 0.001)]
+            [Description("Height of the box")]
+            [StandardControlIcon(BitmapLabelType_e.Thickness1)]
+            public double Height { get; set; } = 0.1;
+
+            [NumberBoxOptions(NumberBoxUnitType_e.Length, 0, 1000, 0.01, false, 0.02, 0.001)]
+            [Description("Length of the box")]
+            [StandardControlIcon(BitmapLabelType_e.Depth)]
+            public double Length { get; set; } = 0.1;
+        }
+
+        public ParametersGroup Parameters { get; }
+
+        public BoxPropertyPage() 
+        {
+            Parameters = new ParametersGroup();
+        }
+    }
+}
--- templates/SwAddIn/__TemplateNamePlaceholder__SwAddin/__TemplateNamePlaceholder__SwAddIn.cs
+++ templates/SwAddIn/__TemplateNamePlaceholder__SwAddin/__TemplateNamePlaceholder__SwAddIn.cs
@@ -1,15 +1,21 @@
 ﻿#if _AddCommandManager_ || _AddPropertyPage_ || _AddCustomFeature_
+using __TemplateNamePlaceholder__SwAddin;
 using __TemplateNamePlaceholder__SwAddin.Properties;
 #endif
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using Xarial.XCad.Base;
 using Xarial.XCad.Base.Attributes;
 using Xarial.XCad.Base.Enums;
+using Xarial.XCad.Features;
 using Xarial.XCad.SolidWorks;
+using Xarial.XCad.SolidWorks.Documents;
 using Xarial.XCad.UI.Commands;
 using Xarial.XCad.UI.Commands.Attributes;
 using Xarial.XCad.UI.Commands.Enums;
+using Xarial.XCad.UI.PropertyPage;
+using Xarial.XCad.UI.PropertyPage.Enums;
 
 namespace __TemplateNamePlaceholder__.Sw.AddIn
 {
@@ -47,14 +53,25 @@ namespace __TemplateNamePlaceholder__.Sw.AddIn
         }
 
 #endif
+#if _AddPropertyPage_
+        private IXPropertyPage<BoxPropertyPage> m_BoxPage;
+        private BoxPropertyPage m_BoxData;
+
+#endif
         public override void OnConnect()
         {
 #if _AddCommandManager_ || _AddPropertyPage_ || _AddCustomFeature_
-            this.CommandManager.AddCommandGroup<Commands_e>().CommandClick += OnCommandClick;
+            CommandManager.AddCommandGroup<Commands_e>().CommandClick += OnCommandClick;
+#if _AddPropertyPage_
+            m_BoxPage = CreatePage<BoxPropertyPage>();
+            m_BoxData = new BoxPropertyPage();
+            m_BoxPage.Closed += OnBoxPageClosed;
+#endif
 #else
             Application.ShowMessageBox("Hello, __TemplateNamePlaceholder__! xCAD.NET", MessageBoxIcon_e.Info);
 #endif
         }
+
 #if _AddCommandManager_ || _AddPropertyPage_ || _AddCustomFeature_
 
         private void OnCommandClick(Commands_e spec)
@@ -64,7 +81,7 @@ namespace __TemplateNamePlaceholder__.Sw.AddIn
 #if _AddCommandManager_ || _AddPropertyPage_
                 case Commands_e.CreateBox:
 #if _AddPropertyPage_
-                    //TODO: Show page
+                    m_BoxPage.Show(m_BoxData);
 #else
                     CreateBox(100, 200, 300);
 #endif
@@ -82,10 +99,49 @@ namespace __TemplateNamePlaceholder__.Sw.AddIn
             }
         }
 #endif
-#if _AddCommandManager_ || _AddPropertyPage_ 
+#if _AddPropertyPage_
+        private void OnBoxPageClosed(PageCloseReasons_e reason)
+        {
+            if (reason == PageCloseReasons_e.Okay)
+            {
+                CreateBox(m_BoxData.Parameters.Width, m_BoxData.Parameters.Height, m_BoxData.Parameters.Length);
+            }
+        }
+
+#endif
+
+#if _AddCommandManager_ || _AddPropertyPage_
 
         private void CreateBox(double width, double height, double length) 
         {
+            var doc = Application.Documents.Active;
+
+            ISwPart part;
+
+            switch (doc) 
+            {
+                case ISwPart activePart:
+                    part = activePart;
+                    break;
+
+                case ISwAssembly assm:
+                    var editingDoc = assm.EditingComponent?.ReferencedDocument;
+                    if (editingDoc is ISwPart)
+                    {
+                        part = (ISwPart)editingDoc;
+                    }
+                    else 
+                    {
+                        throw new NotSupportedException();
+                    }
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+            }
+
+            var sketch = part.Features.PreCreate2DSketch();
+            sketch.Commit();
         }
 #endif
     }
