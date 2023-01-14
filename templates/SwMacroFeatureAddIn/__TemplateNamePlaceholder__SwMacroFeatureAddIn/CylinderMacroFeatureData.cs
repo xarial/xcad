44948224b6d3ce4303ad3acf480908d33f810e6b
Artem <artem.taturevych@xarial.com>
2023-01-14 18:29:27 +1100

    Updated macro feature template to use parameters

--- templates/SwMacroFeatureAddIn/__TemplateNamePlaceholder__SwMacroFeatureAddIn/CylinderMacroFeatureData.cs
+++ templates/SwMacroFeatureAddIn/__TemplateNamePlaceholder__SwMacroFeatureAddIn/CylinderMacroFeatureData.cs
@@ -2,9 +2,12 @@
 using Xarial.XCad.Features.CustomFeature.Attributes;
 using Xarial.XCad.Features.CustomFeature.Enums;
 using Xarial.XCad.Geometry;
+using Xarial.XCad.UI.PropertyPage.Attributes;
+using Xarial.XCad.UI.PropertyPage.Enums;
 
 namespace __TemplateNamePlaceholder__.Sw.AddIn
 {
+#if _SupportsEditBodies_
     public enum BooleanOptions_e
     {
         Extrude,
@@ -15,26 +18,34 @@ namespace __TemplateNamePlaceholder__.Sw.AddIn
         Cut
     }
 
+#endif
     //this corresponds to the data required to generate this macro feature
     public class CylinderMacroFeatureData
     {
         //face or plane. This entity will be associated as the parent relation and macro feature will update
         //when this entity changes
         public IXEntity PlaneOrFace { get; set; }
-        
+
+#if _AddDimensions_
         //marking this parameter to be served as the radial dimension
         [ParameterDimension(CustomFeatureDimensionType_e.Radial)]
+#endif
         public double Radius { get; set; } = 0.1;
 
+#if _AddDimensions_
         //marking this parameter to be served as the linear dimension
         [ParameterDimension(CustomFeatureDimensionType_e.Linear)]
+#endif
         public double Height { get; set; } = 0.1;
 
+        [ControlOptions(align: ControlLeftAlign_e.Indent)]
         public bool Reverse { get; set; } = false;
-
+#if _SupportsEditBodies_
+        
         [ParameterEditBody]
         public IXBody EditBody { get; set; }
-        
+
         public BooleanOptions_e BooleanOptions { get; set; } = BooleanOptions_e.Extrude;
+#endif
     }
 }
--- templates/SwMacroFeatureAddIn/__TemplateNamePlaceholder__SwMacroFeatureAddIn/__TemplateNamePlaceholder__SwMacroFeatureAddIn.cs
+++ templates/SwMacroFeatureAddIn/__TemplateNamePlaceholder__SwMacroFeatureAddIn/__TemplateNamePlaceholder__SwMacroFeatureAddIn.cs
@@ -34,8 +34,13 @@ namespace __TemplateNamePlaceholder__.Sw.AddIn
             [Icon(typeof(Resources), nameof(Resources.cylinder_icon))]
             [Title("Create Cylinder")]
             [Description("Creates parametric cylinder")]
+#if _SupportsInContext_
             [CommandItemInfo(true, true, WorkspaceTypes_e.Part | WorkspaceTypes_e.InContextPart, true)]
             CreateCylinder
+#else
+            [CommandItemInfo(true, true, WorkspaceTypes_e.Part, true)]
+            CreateCylinder
+#endif
         }
 
         //function is called when add-in is loading
@@ -52,7 +57,24 @@ namespace __TemplateNamePlaceholder__.Sw.AddIn
             switch (spec)
             {
                 case Commands_e.CreateCylinder:
+#if _AddEditor_
                     Application.Documents.Active.Features.CreateCustomFeature<CylinderMacroFeatureDefinition, CylinderMacroFeatureData, CylinderPropertyPage>();
+#else
+                    //use the first selected plane or face as the reference
+                    var refPlane = Application.Documents.Active.Selections.OfType<IXEntity>().FirstOrDefault(e => e is IXPlanarRegion);
+                    if (refPlane != null)
+                    {
+                        //if not selected use front plane
+                        refPlane = Application.Documents.Active.Features.OfType<IXPlane>().First();
+                    }
+
+                    Application.Documents.Active.Features.CreateCustomFeature<CylinderMacroFeatureDefinition, CylinderMacroFeatureData>(new CylinderMacroFeatureData()
+                    {
+                        Height = 0.1,
+                        Radius = 0.01,
+                        PlaneOrFace = refPlane,
+                    });
+#endif
                     break;
             }
         }
