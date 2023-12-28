8120bca403db065eea3c08fdbac377cdaf92a3c7
Artem <artem.taturevych@xarial.com>
2023-12-28 18:53:30 +1100

    Updated console templates
    [skip ci]

--- templates/Console/__TemplateNamePlaceholder__Console.Ai/Program.cs
+++ templates/Console/__TemplateNamePlaceholder__Console.Ai/Program.cs
@@ -9,6 +9,8 @@ namespace __TemplateNamePlaceholder__Console.Sw
     {
         static void Main(string[] args)
         {
+            //NOTE: in order to allow the creation of multiple instances of the Inventor application,
+            //register the tools\Xarial.XCad.Inventor.Tools.StandAloneConnector add-in from the NuGet package
             using (var reader = new PropertiesReader(AiApplicationFactory.Create(AiVersion_e.Inventor2023), Console.Out)) 
             {
                 foreach (var filePath in args) 
--- templates/Console/__TemplateNamePlaceholder__Console.Base/PropertiesReader.cs
+++ templates/Console/__TemplateNamePlaceholder__Console.Base/PropertiesReader.cs
@@ -8,17 +8,29 @@ using Xarial.XCad.Documents.Extensions;
 
 namespace __TemplateNamePlaceholder__Console.Base
 {
+    /// <summary>
+    /// Service to print custom properties from file, configuration and cut-list to the output text writer
+    /// </summary>
     public class PropertiesReader : IDisposable
     {
         private readonly IXApplication m_App;
         private readonly TextWriter m_OutWriter;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="app">Pointer to the application</param>
+        /// <param name="outWriter">Text writer to output property values</param>
         public PropertiesReader(IXApplication app, TextWriter outWriter)
         {
             m_App = app;
             m_OutWriter = outWriter;
         }
 
+        /// <summary>
+        /// Prints properties of the specified file
+        /// </summary>
+        /// <param name="filePath">File to read and print properties from</param>
         public void PrintProperties(string filePath) 
         {
             using (var doc = m_App.Documents.Open(filePath, DocumentState_e.ReadOnly)) 
