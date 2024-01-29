4b70c72ae2151062de0d7d98826e0390e650b8a0
Artem Taturevych <artem@xarial.com>
2024-01-30 10:40:25 +1100

    Fixed the planar body object disconnected
    Updated the file header

--- src/Base/Geometry/Surfaces/IXToroidalSurface.cs
+++ src/Base/Geometry/Surfaces/IXToroidalSurface.cs
@@ -1,6 +1,6 @@
 ﻿//*********************************************************************
 //xCAD
-//Copyright(C) 2023 Xarial Pty Limited
+//Copyright(C) 2024 Xarial Pty Limited
 //Product URL: https://www.xcad.net
 //License: https://xcad.xarial.com/license/
 //*********************************************************************
